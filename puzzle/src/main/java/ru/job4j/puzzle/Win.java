c41d0078df73aa45c04b80d7cca84f5a7b88d208
Lesa-89 <lesa-89@mail.ru>
2022-11-22 00:50:43 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,7 +5,8 @@ public class Win {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
-                return true;
+                rsl = true;
+                break;
             }
         }
         return rsl;
